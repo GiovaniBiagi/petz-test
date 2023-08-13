b68ae56043da83236c6ebd8ef34779c84de82110
Giovani Alves <giovani.alves121@gmail.com>
2023-08-13 00:10:17 -0300

    feat: add theme provider to app file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import theme from "@/styles/theme";
+import GlobalStyles from "@/styles/global";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+      <GlobalStyles />
+    </ThemeProvider>
+  );
 }
+
+export default MyApp;
