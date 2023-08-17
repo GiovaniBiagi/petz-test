88bd54a71c781443e1c372ad69141d308313e7b1
Giovani Alves <giovani.alves121@gmail.com>
2023-08-16 23:57:32 -0300

    feat: create Home template

---
+++ templates/Home/Home.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { FOOTER_HEIGHT, HEADER_HEIGHT } from "@/constants/style.constants";
+
+import * as S from "./Home.styles";
+
+export function Home() {
+  return (
+    <S.Container>
+      <S.Image src="/images/pokemon-hero.jpg" alt="Hero" />
+      <S.TitleContainer>
+        <S.Title>Cuidamos bem do seu pokémon,</S.Title>
+        <S.Title>para ele cuidar bem de você</S.Title>
+      </S.TitleContainer>
+    </S.Container>
+  );
+}
