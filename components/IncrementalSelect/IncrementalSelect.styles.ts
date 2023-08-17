8ad639596f32f7c6e862634f1108d20aa4f95968
Giovani Alves <giovani.alves121@gmail.com>
2023-08-17 00:18:16 -0300

    feat: create IncrementalSelect component

---
+++ components/IncrementalSelect/IncrementalSelect.styles.ts
@@ -0,0 +1,47 @@
+import styled, { css } from "styled-components";
+import { FiPlus, FiTrash } from "react-icons/fi";
+
+import { SelectInput } from "@/components/SelectInput/SelectInput";
+import { Button } from "@/components/Button/Button";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  gap: 3.2rem;
+`;
+
+export const Select = styled(SelectInput)``;
+
+export const AddButton = styled(Button)``;
+export const DeleteButton = styled(Button)``;
+
+export const SelectCustomLabel = styled.p`
+  ${({ theme }) => css`
+    margin-right: 1.6rem;
+    font-size: ${theme.font.sizes.xsmall};
+    font-weight: ${theme.font.bold};
+    color: ${theme.colors.text.primary};
+  `}
+`;
+
+export const SelectInputContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const PlusIcon = styled(FiPlus)`
+  ${({ theme }) => css`
+    color: ${theme.colors.text.primary};
+    font-size: ${theme.font.sizes.medium};
+    stroke-width: 3px;
+  `}
+`;
+
+export const TrashIcon = styled(FiTrash)`
+  ${({ theme }) => css`
+    color: ${theme.colors.text.primary};
+    font-size: ${theme.font.sizes.medium};
+    stroke-width: 3px;
+  `}
+`;
