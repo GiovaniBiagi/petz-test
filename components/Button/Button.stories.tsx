44c9ab70a603faaaddc125444b2ab58277ca59a0
Giovani Alves <giovani.alves121@gmail.com>
2023-08-16 23:56:00 -0300

    refactor: Button component

--- components/Button/Button.stories.tsx
+++ components/Button/Button.stories.tsx
@@ -1,8 +1,10 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import { FiPlus } from "react-icons/fi";
 
 import { Button } from "./Button";
 
 const meta: Meta<typeof Button> = {
+  title: "Components/Button",
   component: Button,
 };
 
@@ -21,5 +23,13 @@ export const Secondary: Story = {
   args: {
     label: "Secondary",
     variant: "secondary",
+  },
+};
+
+export const WithIcon: Story = {
+  args: {
+    label: "With Icon",
+    variant: "primary",
+    icon: <FiPlus />,
   },
 };
--- components/Button/Button.styles.ts
+++ components/Button/Button.styles.ts
@@ -4,12 +4,39 @@ import { ButtonProps } from "./Button";
 type StyledButtonProps = Omit<ButtonProps, "label">;
 
 const buttonVariants = {
-  primary: (theme: DefaultTheme) => css``,
-  secondary: (theme: DefaultTheme) => css``,
+  primary: (theme: DefaultTheme) => css`
+    background-color: ${theme.colors.brand.primary};
+    color: ${theme.colors.neutral.white};
+    &:hover {
+      filter: opacity(0.8);
+    }
+  `,
+  secondary: (theme: DefaultTheme) => css`
+    background-color: ${theme.colors.neutral.white};
+    border: 1px solid ${theme.colors.text.primary};
+    color: ${theme.colors.text.primary};
+    &:hover {
+      filter: invert();
+    }
+  `,
+  icon: () => css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.8rem;
+  `,
 };
 
-export const Button = styled.button<StyledButtonProps>`
-  ${({ theme, variant }) => css`
+export const Button = styled.button.attrs({
+  type: "button",
+})<StyledButtonProps>`
+  ${({ theme, variant, icon }) => css`
+    padding: 1.2rem 2.4rem;
+    border-radius: ${theme.radius.full};
+    font-weight: ${theme.font.bold};
+    font-size: ${theme.font.sizes.small};
+
     ${variant && buttonVariants[variant](theme)};
+    ${icon && buttonVariants["icon"]()};
   `}
 `;
